27c14dea53056cfb1f3c7d4d32879b27074f81cd
Clairjay7 <147216829+Clairjay7@users.noreply.github.com>
2024-08-19 14:10:08 +0800

    Update
    Some add.

--- ACTIVITY.cpp
+++ ACTIVITY.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 int main(){
 	
+	int number;
 	cout<<"MAIN MENU"<<endl;
 	cout<<"----------"<<endl;
 	cout<<"1. to Display List of Files"<<endl;
 	cout<<"2. to Create New Directory"<<endl;
 	cout<<"3. to Change the working Directory"<<endl;
-	cout<<"4. Exit";
+	cout<<"4. Exit"<<endl;
+	cout<<"Enter the number: ";
+	cin>>number ;
+	
 
 }
 

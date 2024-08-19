1705805a1aa724fedb8764c218655156e52c71bf
Clairjay7 <147216829+Clairjay7@users.noreply.github.com>
2024-08-19 14:05:48 +0800

    Update
    Starting

---
+++ ACTIVITY.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	cout<<"MAIN MENU"<<endl;
+	cout<<"----------"<<endl;
+	cout<<"1. to Display List of Files"<<endl;
+	cout<<"2. to Create New Directory"<<endl;
+	cout<<"3. to Change the working Directory"<<endl;
+	cout<<"4. Exit";
+
+}
+
+
+
